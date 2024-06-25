6406ebb232f5930df15c2488337f27ff3ec8598f
MSRossii <marcelosrossi13@gmail.com>
2024-06-25 15:54:23 -0300

    add all imgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
-import "./App.css";
-
 function App() {
-  return <></>;
+  return (
+    <div>
+      <img
+        className="image"
+        src="https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2Fyc3xlbnwwfHwwfHx8MA%3D%3D"
+        alt=""
+      />
+      <img
+        className="image"
+        src="https://images.unsplash.com/photo-1514316454349-750a7fd3da3a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2Fyc3xlbnwwfHwwfHx8MA%3D%3D"
+        alt=""
+      />
+      <img
+        className="image"
+        src="https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNhcnN8ZW58MHx8MHx8fDA%3D"
+        alt=""
+      />
+      <img
+        className="image"
+        src="https://images.unsplash.com/photo-1555353540-64580b51c258?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNhcnN8ZW58MHx8MHx8fDA%3D"
+        alt=""
+      />
+      <img
+        className="image"
+        src="https://images.unsplash.com/photo-1616422285623-13ff0162193c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGNhcnN8ZW58MHx8MHx8fDA%3D"
+        alt=""
+      />
+      <img
+        className="image"
+        src="https://images.unsplash.com/photo-1602777924012-f8664ffeed27?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fGNhcnN8ZW58MHx8MHx8fDA%3D"
+        alt=""
+      />
+    </div>
+  );
 }
 
 export default App;
